b7edb992be95d166193ebeef886e173b04ee1e57
AnshuKumari904 <anshukumari904@gmail.com>
2023-11-01 15:57:34 +0530

    Added new components
    Added new components

--- travel-blog-app/src/app/app-routing.module.ts
+++ travel-blog-app/src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { CategoryNavbarComponent } from './layouts/category-navbar/category-navbar.component';
+import { SingleCategoryComponent } from './pages/single-category/single-category.component';
+import { SingleOstComponent } from './pages/single-ost/single-ost.component';
+import { SinglePostComponent } from './pages/single-post/single-post.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ContactUsComponent } from './pages/contact-us/contact-us.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:HomeComponent},
+  {path:'category',component:SingleCategoryComponent},
+  {path:'post',component:SinglePostComponent},
+  {path:'about',component:AboutComponent},
+  {path:'contact',component:ContactUsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
